5e09a9b134c6b49bafd25fd087407d63557d17b6
nesmartinez1 <nestorandres04@gmail.com>
2025-06-04 17:29:15 -0400

    project update

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,47 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import './App.css'
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+export default function App() {
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
+    const [count, setCount] = useState(0)
 
-    componentDidMount() {
+    useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
-        .then(response => response.json())
-        .then(users => this.setState({robots: users}))
+            .then(response => response.json())
+            .then(users => setRobots(users))
+    }, []) // empty array means run useEffect once (componentDidMount), [searchfield] means run use effect when searchfield changes
+
+    useEffect(() => {
+        console.log(count)
+    }, [count])
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield:event.target.value})
-    }
-
-    render() {
-        const { robots, searchfield } = this.state // <<<
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
-        if (robots.length === 0) { // or if (!robots.length), you could also use a ternary operator here instead of if/else
-            return <h1>Loading...</h1> // Loading component
-        } else {
-            return (
-                <div className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <CardList robots={filteredRobots}/>
-                    </Scroll>
-                </div>
-            );   
-        }
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
+    if (robots.length === 0) { // or if (!robots.length), you could also use a ternary operator here instead of if/else
+        return <h1>Loading...</h1> // Loading component
+    } else {
+        return (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                {/* <button onClick={() => setCount(count + 1)}>Click Me</button> */}
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        );   
     }
 }
-
-export default App
